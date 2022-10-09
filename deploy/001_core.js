ab8f449fbf1df7ebe84c7bb9f90bc78bd8237599
gianmarcoalarcon <gianmarcoalarcon>
2022-10-09 00:10:32 -0500

    feat: update deploy files

--- deploy/001_core.js
+++ deploy/001_core.js
@@ -6,28 +6,45 @@ const { getTokenAddresses, isFork, isScript } = require("../utils/helpers");
 const {
   deployWithConfirmation,
   withConfirmation,
-  log
+  log,
 } = require("../utils/deploy");
 
 const deployDummyToken = async () => {
   const { deployerAddr, governorAddr } = await getNamedAccounts();
-  await deployWithConfirmation("DummyToken",["USD Tether","USDT"]);
+  await deployWithConfirmation("DummyToken", ["USD Tether", "USDT"]);
+};
+
+const deployEVToken = async () => {
+  const { deployerAddr, governorAddr } = await getNamedAccounts();
+  await deployWithConfirmation("EVToken", ["EV Token", "EVT"]);
 };
 
 const deployJomEV = async () => {
-
-  const {deployerAddr} = await getNamedAccounts();
+  const { deployerAddr } = await getNamedAccounts();
   const sDeployer = await ethers.provider.getSigner(deployerAddr);
   await deployWithConfirmation("JomEV");
   const cJomEV = await ethers.getContract("JomEV");
   const cDummyToken = await ethers.getContract("DummyToken");
   await cJomEV.connect(sDeployer).addAcceptedPayment(cDummyToken.address);
+};
 
+const deployVault = async () => {
+  const { deployerAddr } = await getNamedAccounts();
+  const sDeployer = await ethers.provider.getSigner(deployerAddr);
+  await deployWithConfirmation("Vault");
+  const cVault = await ethers.getContract("Vault");
+  const cDummyToken = await ethers.getContract("DummyToken");
+  const cEVToken = await ethers.getContract("EVToken");
+  const cJomEV = await ethers.getContract("JomEV");
+  await cVault.connect(sDeployer).setEVTokenAddress(cEVToken.address);
+  await cJomEV.connect(sDeployer).setVault(cVault.address);
 };
 
 const main = async () => {
   await deployDummyToken();
+  await deployEVToken();
   await deployJomEV();
+  await deployVault();
 };
 
 main.id = "001_core";
